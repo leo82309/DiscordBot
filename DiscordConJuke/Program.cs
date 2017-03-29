b4d1690ebe317dfe7c31feb456a015153d8b828d
Leo82309 <leo82309@yahoo.com>
2017-03-28 21:28:55 -0700

    Add files via upload

---
+++ DiscordConJuke/Program.cs
@@ -0,0 +1,49 @@
+﻿using Discord.WebSocket;
+using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordConJuke
+{
+
+    public class Program
+    {
+        // Convert our sync main to an async main.
+        public static void Main(string[] args) =>
+            new Program().Start().GetAwaiter().GetResult();
+
+        private DiscordSocketClient client;
+        private CommandHandler handler;
+
+        public async Task Start()
+        {
+            // Define the DiscordSocketClient
+            client = new DiscordSocketClient();
+
+            var token = "MTg0ODY4NjEwNDc1Mjk0NzIw.CiauqQ.SCW7Zzp-oHwiwmzZLtKQeYPcqxk";
+
+            // Login and connect to Discord.
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            var map = new DependencyMap();
+            map.Add(client);
+
+            handler = new CommandHandler();
+            await handler.Install(map);
+
+            // Block this program until it is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.WhenAll();
+        }
+    }
+}
